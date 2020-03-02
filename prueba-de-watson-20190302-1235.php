ec3d9822bc16d611d8654f29b48870d4c12c9d67
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2020-03-02 14:31:25 -0500

    ver mejor el Json.
    Se ha añadido una funcionalidad para ver mejor el Json.

--- prueba-de-watson-20190302-1235.php
+++ prueba-de-watson-20190302-1235.php
@@ -10,15 +10,15 @@
     
     $image_url = "&url=".$file;
     
-    $headers = array( 'Content-Type: application/json' );
+    //$headers = array( 'Content-Type: application/json' );
     
     //cURL
     $ch = curl_init();
     
-        curl_setopt($ch, CURLOPT_USERPWD, 'apikey:+++++++++++++'); 
+        curl_setopt($ch, CURLOPT_USERPWD, 'apikey:jgA_KwDJMrOkJHWU--BKdbmkxTEBOcHtqF54BxaglWE5'); 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
         curl_setopt($ch, CURLOPT_URL, $url."&images_file=".$file ); //Endpoint URL                
-        curl_setopt($ch, CURLOPT_HEADER, $headers ); //Este headers ofrece más información que el otro formato.
+        //curl_setopt($ch, CURLOPT_HEADER, $headers ); //Este headers ofrece más información que el otro formato.
         curl_setopt($ch, CURLOPT_POST, false ); //POST        
         curl_setopt($ch, CURLOPT_POSTFIELDS, $image_url );        
     
@@ -31,8 +31,47 @@
         echo 'Error: '.curl_error($ch);
     }
     
-    echo $result;
+    //echo $result;    
+    //echo "<br><br>";    
+    var_dump( json_decode( $result )); //Aquí es solo para mostrar el tipo de variable y su contenido. 
+    echo "<hr>";
+    
+    $objeto = json_decode( $result, true );    //Convierte Json a un arreglo.
+    //print_r( $objeto[ "classifiers" ] ); //A partir de acá se convierte en un arreglo.
+    
+    //echo $objeto[ 'class' ];
+    
+    foreach ($objeto as $product)
+    {
+        echo '<pre>';
+        print_r($product);
+        echo '</pre>';
+    }
     
     // Close the command
     curl_close ($ch);
+    
+    
+    
+    
+    /*function imprimir_arreglo( $arreglo )
+    {
+        $salida = "";
+        
+        for( $i = 0; $i < count( $arreglo ); $i ++ )
+        {
+            echo "<br>";
+            print_r( $arreglo[ $i ] );
+            
+            //$objeto = json_decode( $arreglo[ $i ] );    
+            //print_r( $objeto->{ "images" } ); //A partir de acá se convierte en un arreglo.
+            
+            //imprimir_arreglo( $objeto->{ "images" } );
+        }
+        
+        //return $salida;
+    }*/
+    
+    
+    
     
